3cb9c366b4022aed35efbb76458c68024b0e8c81
AbrahamDN <abe.ndinga@gmail.com>
2023-06-10 05:03:59 +0100

    feat: Sanity product schema & seed product data

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+    "skQUbyQZIDgAfBFq3KpJd5tXEQTO9GpR3QyqMv5reT8LUwKWe6qMjgMs4UJfk7vVJvWPXrg3Kjs0PBDrk7VYhYYY7MRc6zZ6EmSXh4PD8aH8R5Xpljoy9rYHI1HlEPrhAz0Cnd6OrH2SWj9ecCSWoxsp467BybbsvykhzORMp2kML06hEKcZ",
 })
