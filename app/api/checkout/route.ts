4646c24e63b3076019e23cd7b8241a1830542fa9
AbrahamDN <abe.ndinga@gmail.com>
2023-06-11 11:47:03 +0100

    feat: stripe payment checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,4 +5,28 @@ import { validateCartItems } from "use-shopping-cart/utilities"
 import { inventory } from "@/config/inventory"
 import { stripe } from "@/lib/stripe"
 
-export async function POST() {}
+export async function POST(req: Request) {
+  const cartDetails = await req.json()
+  const lineItems = validateCartItems(inventory, cartDetails)
+  const origin = req.headers.get("origin")
+
+  const session = await stripe.checkout.sessions.create({
+    submit_type: "pay",
+    mode: "payment",
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    shipping_address_collection: {
+      allowed_countries: ["US", "GB"],
+    },
+    shipping_options: [
+      {
+        shipping_rate: "shr_1NHkq3HnxwJv90y7mDEV3nqr",
+      },
+    ],
+    billing_address_collection: "auto",
+    success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${origin}/cart`,
+  })
+
+  return NextResponse.json(session)
+}
